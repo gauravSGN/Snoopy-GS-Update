296a52e6c6b99a5b413934017f5a7b8e0f0a1f3b
Ray Ryan <rayr@sgn.com>
2016-06-01 16:47:09 -0500

    Made BubbleQueue smarter

--- Assets/Scripts/Base/Model/BubbleQueue.cs
+++ Assets/Scripts/Base/Model/BubbleQueue.cs
@@ -1,8 +1,38 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class BubbleQueue
 {
+    private List<BubbleType> queued = new List<BubbleType>();
+
     public BubbleType GetNext()
+    {
+        BubbleType next;
+
+        if (queued.Count == 0)
+        {
+            next = GenerateElement();
+        }
+        else
+        {
+            next = queued[0];
+            queued.RemoveAt(0);
+        }
+
+        return next;
+    }
+
+    public BubbleType Peek(int index)
+    {
+        while (queued.Count < index)
+        {
+            queued.Add(GenerateElement());
+        }
+
+        return queued[index];
+    }
+
+    private BubbleType GenerateElement()
     {
         return (BubbleType)Random.Range(0, 4);
     }
