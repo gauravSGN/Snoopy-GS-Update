aa287fc39e31a470d9c77b5c743607a7cf31b2a3
Ray Ryan <rayr@sgn.com>
2016-06-01 14:42:58 -0500

    Adding an EventDispatcher component

---
+++ Assets/Scripts/Base/Event/EventDispatcher.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class EventDispatcher : MonoBehaviour
+{
+    public EventDispatcher Instance { get { return instance; } }
+
+    private static EventDispatcher instance;
+
+    private Dictionary<Type, List<object>> handlers = new Dictionary<Type, List<object>>();
+
+    public void OnEnable()
+    {
+        instance = this;
+    }
+
+    public void OnDestroy()
+    {
+        if (instance == this)
+        {
+            instance = null;
+        }
+    }
+
+    public void AddEventHandler<T>(Action<T> handler) where T : IGameEvent
+    {
+        var eventType = typeof(T);
+        List<object> handlerList;
+
+        if (handlers.ContainsKey(eventType))
+        {
+            handlerList = handlers[eventType];
+        }
+        else
+        {
+            handlerList = new List<object>();
+            handlers.Add(eventType, handlerList);
+        }
+
+        handlerList.Add(handler);
+    }
+
+    public void RemoveEventHandler<T>(Action<T> handler) where T : IGameEvent
+    {
+        var eventType = typeof(T);
+
+        if (handlers.ContainsKey(eventType))
+        {
+            var handlerList = handlers[eventType];
+
+            if (handlerList.Contains(handler))
+            {
+                handlerList.Remove(handler);
+            }
+        }
+    }
+
+    public void Dispatch<T>(T gameEvent) where T : IGameEvent
+    {
+        var eventType = typeof(T);
+
+        if (handlers.ContainsKey(eventType))
+        {
+            var handlerList = handlers[eventType];
+
+            foreach (var handler in handlerList)
+            {
+                (handler as Action<T>).Invoke(gameEvent);
+            }
+        }
+    }
+}
