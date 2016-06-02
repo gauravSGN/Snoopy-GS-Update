9b4096fdd7ede48df1cacc26aa0f3f4223c9a17f
Daniel Cash <daniel@sgn.com>
2016-06-02 16:30:38 -0500

    Adding in BubbleScore component and BubbleDestroyedEvent.

---
+++ Assets/Scripts/Base/Component/Bubble/BubbleScore.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleScore : MonoBehaviour
+{
+    public Bubble Model { get; private set; }
+
+    public void SetModel(Bubble model)
+    {
+        Model = model;
+    }
+
+    public void OnDestroy()
+    {
+        EventDispatcher.Instance.Dispatch(new BubbleDestroyedEvent(Model.definition.score));
+    }
+}
---
+++ Assets/Scripts/Base/Event/Bubble/BubbleDestroyedEvent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleDestroyedEvent : IGameEvent
+{
+    public int score;
+
+    public BubbleDestroyedEvent(int bubbleScore)
+    {
+        score = bubbleScore;
+    }
+}
