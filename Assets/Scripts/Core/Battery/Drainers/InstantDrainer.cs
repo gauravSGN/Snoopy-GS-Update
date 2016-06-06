3a2c1a69290b9487c573ad9b5a8012af60091f8e
Jason Young <jasony@sgn.com>
2016-06-06 13:59:03 -0700

    Add battery and gizmo code

---
+++ Assets/Scripts/Core/Battery/Drainers/InstantDrainer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class InstantDrainer : MonoBehaviour {
+	public Battery battery;
+	public float percentDrain;
+	public bool startOnEnable;
+
+	void OnEnable() {
+		if(startOnEnable) {
+			StartDrain();
+		}
+	}
+
+	public void StartDrain() {
+		float final = percentDrain * battery.totalCapacity;
+		battery.Sub(final);
+	}
+}
