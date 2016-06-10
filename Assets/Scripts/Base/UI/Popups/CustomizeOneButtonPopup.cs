9267c92c481b01f4b102b9a41548148cc60ecc3b
Jason Young <jasony@sgn.com>
2016-06-10 14:43:23 -0700

    Add title text setting for popup

--- Assets/Scripts/Base/UI/Popups/CustomizeOneButtonPopup.cs
+++ Assets/Scripts/Base/UI/Popups/CustomizeOneButtonPopup.cs
@@ -5,16 +5,19 @@ using System.Collections.Generic;
 public class CustomizeOneButtonPopup : MonoBehaviour, CustomizePopup
 {
     [SerializeField]
-    private Text body;
+    private Text bodyText;
     [SerializeField]
     private Text buttonText;
+    [SerializeField]
+    private Text titleText;
     [SerializeField]
     private Image mainImage;
 
     public void CustomizeText(Dictionary<string, string> data)
     {
-        body.text = data["body"];
+        bodyText.text = data["body"];
         buttonText.text = data["button_label"];
+        titleText.text = data["title"];
     }
 
     public void CustomizeImages(Dictionary<string, Sprite> data)
