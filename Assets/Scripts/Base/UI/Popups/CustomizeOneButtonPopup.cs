4c1d2e0efa297d8a4f8e9648f828cdaae9c38ffe
Jason Young <jasony@sgn.com>
2016-06-10 13:00:44 -0700

    Update popup setup

---
+++ Assets/Scripts/Base/UI/Popups/CustomizeOneButtonPopup.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class CustomizeOneButtonPopup : MonoBehaviour, CustomizePopup
+{
+    [SerializeField]
+    private Text body;
+    [SerializeField]
+    private Text buttonText;
+    [SerializeField]
+    private Image mainImage;
+
+    public void CustomizeText(Dictionary<string, string> data)
+    {
+        body.text = data["body"];
+        buttonText.text = data["button_label"];
+    }
+
+    public void CustomizeImages(Dictionary<string, Sprite> data)
+    {
+        mainImage.sprite = data["main_image"];
+    }
+}
