fe850351a9c107a05e60d10134c2b9fba2689fed
Nick Cash <nickc@sgn.com>
2016-06-16 11:52:17 -0500

    Initial JAR setup

---
+++ Extension/src/main/java/com/sgn/snoopypop/state/State.java
@@ -0,0 +1,13 @@
+package com.sgn.snoopypop.state;
+
+import static com.sgn.gs.data.DataUtils.loc;
+
+public class State
+{
+    private final Object state;
+
+    public State(Object state)
+    {
+        this.state = state;
+    }
+}
