26f3c0f78caf233780431c1d45227e52ed548709
Nick Cash <nickc@sgn.com>
2016-06-17 15:35:43 -0500

    Better command line arguments

--- Assets/Editor/AutomatedBuilder.cs
+++ Assets/Editor/AutomatedBuilder.cs
@@ -4,6 +4,10 @@ using UnityEditor;
 
 public static class AutomatedBuilder
 {
+    private static string[] arguments = Environment.GetCommandLineArgs();
+
+    private const string ANDROID_KEYSTORE_PASS = "-keystorePass";
+    private const string ANDROID_KEYALIAS_PASS = "-keyaliasPass";
     private const string OUTPUT_PATH_COMMAND_LINE_ARGUMENT_NAME = "-outputPath";
 
     static void BuildWindowsDesktop()
@@ -23,32 +27,47 @@ public static class AutomatedBuilder
 
     static void BuildAndroid()
     {
+        PlayerSettings.keystorePass = GetCommandLineArgument(ANDROID_KEYSTORE_PASS);
+        PlayerSettings.keyaliasPass = GetCommandLineArgument(ANDROID_KEYALIAS_PASS);
+
         Build("Builds/Android/App.apk", BuildTarget.Android);
     }
 
     private static void Build(string defaultPath, BuildTarget target)
     {
-        var path = defaultPath;
-        var arguments = Environment.GetCommandLineArgs();
-
-        for (var i = 0; i < arguments.Length; i++)
-        {
-            if ((arguments[i] == OUTPUT_PATH_COMMAND_LINE_ARGUMENT_NAME) && (i + 1 < arguments.Length))
-            {
-                path = arguments[i + 1];
-                break;
-            }
-        }
-
+        var commandLinePath = GetCommandLineArgument(OUTPUT_PATH_COMMAND_LINE_ARGUMENT_NAME);
+        var path = (commandLinePath != "") ? commandLinePath : defaultPath;
         Console.WriteLine("Output Path: " + path);
 
         Console.WriteLine("Starting build...");
         BuildPipeline.BuildPlayer(GetScenePaths(), path, target, BuildOptions.None);
+
         Console.WriteLine("Done");
     }
 
     private static string[] GetScenePaths()
     {
         return EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path).ToArray();
+    }
+
+    private static string GetCommandLineArgument(string argument)
+    {
+        string argumentValue = "";
+
+        for (var index = 0; index < arguments.Length; index++)
+        {
+            if (arguments[index] == argument)
+            {
+                var nextIndex = index + 1;
+
+                if ((nextIndex < arguments.Length) && (arguments[nextIndex][0] != '-'))
+                {
+                    argumentValue = arguments[nextIndex];
+                    break;
+                }
+            }
+        }
+
+        return argumentValue;
     }
 }
