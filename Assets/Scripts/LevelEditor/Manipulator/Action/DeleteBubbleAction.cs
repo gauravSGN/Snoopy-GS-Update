db0d9220898fbb862ec71464c292dd1d28c7d7f2
Ray Ryan <rayr@sgn.com>
2016-06-20 15:13:49 -0500

    Adding delete bubble action

---
+++ Assets/Scripts/LevelEditor/Manipulator/Action/DeleteBubbleAction.cs
@@ -0,0 +1,11 @@
+﻿namespace LevelEditor.Manipulator
+{
+    [ManipulatorAction(ManipulatorActionType.DeleteBubble)]
+    public class DeleteBubbleAction : ManipulatorAction
+    {
+        public void Perform(LevelManipulator manipulator, int x, int y)
+        {
+            manipulator.RemoveBubble(x, y);
+        }
+    }
+}
