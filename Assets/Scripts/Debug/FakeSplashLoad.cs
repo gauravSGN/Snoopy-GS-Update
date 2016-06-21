a94bd15665349cad8ce9c73835c64a8a02a36f76
Jason Young <jasony@sgn.com>
2016-06-21 16:55:49 -0700

    Add fake loading script

---
+++ Assets/Scripts/Debug/FakeSplashLoad.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FakeSplashLoad : MonoBehaviour
+{
+    [SerializeField]
+    private Slider fillBar;
+    [SerializeField]
+    private Button playButton;
+    private float progress;
+    [SerializeField]
+    private float fillPerSecond = 0;
+    private float delay = 0.1f;
+
+    void Start()
+    {
+        playButton.gameObject.SetActive(false);
+        StartCoroutine(FakeLoad());
+    }
+
+    private IEnumerator FakeLoad()
+    {
+        while(progress < 1)
+        {
+            yield return new WaitForSeconds(delay);
+            progress += (fillPerSecond * delay);
+            fillBar.value = progress;
+        }
+        fillBar.gameObject.SetActive(false);
+        playButton.gameObject.SetActive(true);
+    }
+}
