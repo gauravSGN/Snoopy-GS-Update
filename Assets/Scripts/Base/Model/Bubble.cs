66efee57f863f788f57c167b088b781f39a623b9
Daniel Cash <daniel@sgn.com>
2016-06-22 15:45:45 -0500

    Axe some dead static functions from Bubble.cs

--- Assets/Scripts/Base/Model/Bubble.cs
+++ Assets/Scripts/Base/Model/Bubble.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System;
+﻿using System;
 using Graph;
 
 [Serializable]
@@ -49,53 +47,6 @@ public class Bubble : GraphElement<Bubble>
         if (OnDisconnected != null)
         {
             OnDisconnected(this);
-        }
-    }
-
-    private static void CheckForFallingBubbles(List<Bubble> detached)
-    {
-        var connected = new Dictionary<Bubble, bool>();
-
-        foreach (var bubble in detached)
-        {
-            ScanForRoots(bubble, connected);
-            foreach (var root in connected.Where(p => p.Value).ToList())
-            {
-                PropogateRoot(connected, root.Key);
-            }
-        }
-
-        foreach (var pair in connected)
-        {
-            if (!pair.Value)
-            {
-                BubbleReactionEvent.Dispatch(ReactionPriority.Cull, pair.Key.MakeBubbleFall);
-            }
-        }
-    }
-
-    private static void ScanForRoots(Bubble bubble, Dictionary<Bubble, bool> connected)
-    {
-        if (!connected.ContainsKey(bubble))
-        {
-            connected.Add(bubble, bubble.IsRoot);
-
-            foreach (var neighbor in bubble.neighbors)
-            {
-                ScanForRoots(neighbor, connected);
-            }
-        }
-    }
-
-    private static void PropogateRoot(Dictionary<Bubble, bool> connected, Bubble root)
-    {
-        foreach (var neighbor in root.neighbors)
-        {
-            if (!connected[neighbor])
-            {
-                connected[neighbor] = true;
-                PropogateRoot(connected, neighbor);
-            }
         }
     }
 }
