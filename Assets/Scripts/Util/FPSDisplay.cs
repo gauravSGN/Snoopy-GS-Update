8f06252730cadfbd0a2cc7cabb066001ab2ea02f
Jason Young <jasony@sgn.com>
2016-06-24 12:07:06 -0700

    Add defines and make FPSDisplay only compile for debug

--- Assets/Scripts/Util/FPSDisplay.cs
+++ Assets/Scripts/Util/FPSDisplay.cs
@@ -3,33 +3,35 @@ using System.Collections;
 
 public class FPSDisplay : MonoBehaviour
 {
+#if (DEBUG)
     private const float updateDelay = 0.1f;
-	private float deltaTime;
+    private float deltaTime;
     private float delayTimer;
 
-	void Update()
-	{
+    void Update()
+    {
         delayTimer += Time.deltaTime;
         if(delayTimer >=  updateDelay)
         {
             deltaTime = Time.deltaTime;
             delayTimer = 0;
         }
-	}
+    }
 
-	void OnGUI()
-	{
-		int w = Screen.width, h = Screen.height;
+    void OnGUI()
+    {
+        int w = Screen.width, h = Screen.height;
 
-		GUIStyle style = new GUIStyle();
+        GUIStyle style = new GUIStyle();
+        style.alignment = TextAnchor.UpperLeft;
+        style.fontSize = h * 2 / 100;
+        style.normal.textColor = new Color (1.0f, 1.0f, 1.0f, 1.0f);
 
-		Rect rect = new Rect(0, 0, w, h * 2 / 100);
-		style.alignment = TextAnchor.UpperLeft;
-		style.fontSize = h * 2 / 100;
-		style.normal.textColor = new Color (1.0f, 1.0f, 1.0f, 1.0f);
-		float msec = deltaTime * 1000.0f;
-		float fps = 1f / deltaTime;
-		string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
-		GUI.Label(rect, text, style);
-	}
+        float msec = deltaTime * 1000.0f;
+        float fps = 1f / deltaTime;
+        string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+        Rect rect = new Rect(0, 0, w, h * 2 / 100);
+        GUI.Label(rect, text, style);
+    }
+#endif
 }
