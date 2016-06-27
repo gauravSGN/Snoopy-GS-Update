1bb609970dc6967adb889864770e324512ae3c4d
Ray Ryan <rayr@sgn.com>
2016-06-27 11:44:56 -0500

    Updating BubbleSnap to use GameConfig values

--- Assets/Scripts/Base/Component/Bubble/BubbleSnap.cs
+++ Assets/Scripts/Base/Component/Bubble/BubbleSnap.cs
@@ -4,9 +4,6 @@ using System.Collections.Generic;
 
 public class BubbleSnap : MonoBehaviour
 {
-    private const float BUBBLE_SPACING = 0.25f;
-    private const float RADIUS_FACTOR = 0.5f;
-
     private Rigidbody2D rigidBody;
     private new CircleCollider2D collider;
 
@@ -16,7 +13,7 @@ public class BubbleSnap : MonoBehaviour
         rigidBody = GetComponent<Rigidbody2D>();
         collider = GetComponent<CircleCollider2D>();
 
-        collider.radius *= RADIUS_FACTOR;
+        collider.radius *= GlobalState.Instance.Config.bubbles.shotColliderScale;
     }
 
     protected void OnCollisionEnter2D(Collision2D collision)
@@ -34,7 +31,7 @@ public class BubbleSnap : MonoBehaviour
             rigidBody.gravityScale = 1.0f;
             rigidBody.isKinematic = true;
 
-            collider.radius /= RADIUS_FACTOR;
+            collider.radius /= GlobalState.Instance.Config.bubbles.shotColliderScale;
             gameObject.layer = (int)Layers.GameObjects;
 
             Destroy(this);
@@ -72,7 +69,7 @@ public class BubbleSnap : MonoBehaviour
 
     private IEnumerable<Collider2D> NearbyBubbles(Vector2 location)
     {
-        foreach (var hit in Physics2D.CircleCastAll(location, BUBBLE_SPACING, Vector2.up, 0.0f))
+        foreach (var hit in Physics2D.CircleCastAll(location, GlobalState.Instance.Config.bubbles.size, Vector2.up, 0.0f))
         {
             if ((hit.collider.gameObject != gameObject) && (hit.collider.gameObject.tag == StringConstants.Tags.BUBBLES))
             {
@@ -83,8 +80,9 @@ public class BubbleSnap : MonoBehaviour
 
     private bool CanPlaceAtLocation(Vector2 location)
     {
+        var halfSize = GlobalState.Instance.Config.bubbles.size / 2.0f;
 
-        foreach (var hit in Physics2D.CircleCastAll(location, (BUBBLE_SPACING / 2.0f) * 0.9f, Vector2.up, 0.0f,
+        foreach (var hit in Physics2D.CircleCastAll(location, halfSize * 0.9f, Vector2.up, 0.0f,
                                                     (1 << (int)Layers.GameObjects | 1 << (int)Layers.Walls)))
         {
             if (hit.collider.gameObject != gameObject)
@@ -99,6 +97,7 @@ public class BubbleSnap : MonoBehaviour
     private IEnumerable<Vector2> GetAttachmentPoints(IEnumerable<GameObject> bubbles)
     {
         var theta = Mathf.PI / 3.0f;
+        var bubbleSize = GlobalState.Instance.Config.bubbles.size;
 
         foreach (var bubble in bubbles)
         {
@@ -107,8 +106,8 @@ public class BubbleSnap : MonoBehaviour
             for (var index = 0; index < 6; index++)
             {
                 yield return new Vector2(
-                    bubblePosition.x + Mathf.Cos(index * theta) * BUBBLE_SPACING,
-                    bubblePosition.y + Mathf.Sin(index * theta) * BUBBLE_SPACING
+                    bubblePosition.x + Mathf.Cos(index * theta) * bubbleSize,
+                    bubblePosition.y + Mathf.Sin(index * theta) * bubbleSize
                 );
             }
         }
