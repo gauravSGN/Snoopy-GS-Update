d916469cff3872d6bdeb910c1d81c4636c61fdec
Jason Young <jasony@sgn.com>
2016-06-27 09:27:35 -0700

    Add loading screen prefab and code

---
+++ Assets/Scripts/UI/AsyncLoadingFill.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AsyncLoadingFill : MonoBehaviour
+{
+    [SerializeField]
+    private Slider fillBar;
+
+    public void StartFill(AsyncOperation op)
+    {
+        StartCoroutine(ProgressFill(op));
+    }
+
+    private IEnumerator ProgressFill(AsyncOperation op)
+    {
+        while (!op.isDone)
+        {
+            fillBar.value = op.progress;
+            yield return new WaitForSeconds(0.1f);
+        }
+        gameObject.SetActive(false);
+    }
+}
