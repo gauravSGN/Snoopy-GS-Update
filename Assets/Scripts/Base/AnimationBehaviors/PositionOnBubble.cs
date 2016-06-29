678761cb73ce0a2b73f76ff68134f3193b26c666
Daniel Cash <daniel@sgn.com>
2016-06-29 16:28:29 -0500

    Even better fix for spacing issue.

--- Assets/Scripts/Base/AnimationBehaviors/PositionOnBubble.cs
+++ Assets/Scripts/Base/AnimationBehaviors/PositionOnBubble.cs
@@ -4,6 +4,6 @@ public class PositionOnBubble : StateMachineBehaviour
 {
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.gameObject.transform.localPosition = new Vector3(0, 0, -1);
+        animator.gameObject.transform.localPosition = Vector3.back;
     }
 }
