06567067a7b9f9951de29706db050785a8434ec2
Jason Young <jasony@sgn.com>
2016-07-07 15:40:32 -0700

    Add object startup debug utils

---
+++ Assets/Scripts/Debug/StartupTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class StartupTest : MonoBehaviour
+{
+    public string name;
+
+    void Awake()
+    {
+        Debug.Log(name+" Awake");
+    }
+
+    void OnEnable()
+    {
+        Debug.Log(name+" Enable");
+    }
+
+    void Start()
+    {
+        Debug.Log(name+" Start");
+    }
+}
