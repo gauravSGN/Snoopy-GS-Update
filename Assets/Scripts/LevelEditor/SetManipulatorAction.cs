928f9e2edc57a19bddbb874140486b142ad56354
Ray Ryan <rayr@sgn.com>
2016-07-12 10:50:38 -0500

    SPOP-374 Adding a delete button

---
+++ Assets/Scripts/LevelEditor/SetManipulatorAction.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using LevelEditor.Manipulator;
+
+namespace LevelEditor
+{
+    public class SetManipulatorAction : MonoBehaviour
+    {
+        [SerializeField]
+        private LevelManipulator manipulator;
+
+        [SerializeField]
+        private ManipulatorActionType type;
+
+        private Button button;
+
+        public void Start()
+        {
+            button = GetComponent<Button>();
+            button.onClick.AddListener(OnClick);
+        }
+
+        private void OnClick()
+        {
+            manipulator.SetActionType(type);
+        }
+    }
+}
