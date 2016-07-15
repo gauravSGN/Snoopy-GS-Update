159e6d324bc04ffdd57af36a9d549ed2c0a31c7b
Ray Ryan <rayr@sgn.com>
2016-07-15 14:55:11 -0500

    Moving shot count from tuning panel to bubble queue panel and renaming to "Move Count"

--- Assets/Scripts/LevelEditor/Properties/LevelProperties.cs
+++ Assets/Scripts/LevelEditor/Properties/LevelProperties.cs
@@ -10,29 +10,24 @@ namespace LevelEditor.Properties
             new string[] { "3484d8", "ea9115", "d22d2b", "11952a", "e746a1", "891fc4" })]
         public float[] PowerUpFills { get; set; }
 
-        public int ShotCount { get; set; }
-
         [PropertyDisplayAttribute(
             new string[] { "Star 1 Score", "Star 2 Score", "Star 3 Score" })]
         public int[] StarValues { get; set; }
 
         public LevelProperties()
         {
-            ShotCount = 20;
             StarValues = new int[] { 100, 500, 1000 };
             PowerUpFills = new float[6];
         }
 
         public void FromLevelData(LevelData data)
         {
-            ShotCount = data.ShotCount;
             StarValues = data.StarValues;
             PowerUpFills = data.PowerUpFills;
         }
 
         public void ToLevelData(MutableLevelData data)
         {
-            data.ShotCount = ShotCount;
             data.StarValues = StarValues;
             data.PowerUpFills = PowerUpFills;
         }
