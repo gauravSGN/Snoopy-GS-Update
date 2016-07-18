bf03fb05a837214b0bfa9c8a3c0c81f979c2de77
Ray Ryan <rayr@sgn.com>
2016-07-18 11:03:12 -0500

    SPOP-379 Adding a keyboard shortcut handler to return to the level editor when testing levels

---
+++ Assets/Scripts/LevelEditor/ReturnToEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using Service;
+
+namespace LevelEditor
+{
+    public class ReturnToEditor : MonoBehaviour
+    {
+        public void Return()
+        {
+            SceneManager.LoadScene(GlobalState.Instance.Services.Get<SceneService>().ReturnScene);
+        }
+
+        protected void OnLevelWasLoaded(int level)
+        {
+            if (SceneManager.GetActiveScene().name == LevelEditorConstants.SCENE_NAME)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
