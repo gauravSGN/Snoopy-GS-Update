965dfb35f365a12fe06effe5da1a35c5f57429af
Ray Ryan <rayr@sgn.com>
2016-07-27 11:05:50 -0500

    SPOP-475 Adding a sequential coroutine extension method and updating ReactionLogic to use it

---
+++ Assets/Scripts/Util/Extensions/MonoBehaviourExtensions.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ExtensionMethods
+{
+    static public class MonoBehaviourExtensions
+    {
+        static public void StartCoroutinesSequential(this MonoBehaviour behaviour, IEnumerable<IEnumerator> coroutines)
+        {
+            behaviour.StartCoroutine(RunSequential(coroutines));
+        }
+
+        static private IEnumerator RunSequential(IEnumerable<IEnumerator> coroutines)
+        {
+            foreach (var coroutine in coroutines)
+            {
+                while (coroutine.MoveNext())
+                {
+                    yield return coroutine.Current;
+                }
+
+                yield return null;
+            }
+        }
+    }
+}
