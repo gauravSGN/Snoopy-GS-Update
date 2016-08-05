e1c172bc5688b65a285299e9daab389d85229419
Jason Young <jasony@sgn.com>
2016-08-04 17:03:57 -0700

    SPOP-538 Initial texture swap button work

---
+++ Assets/Scripts/Debug/SwapActiveSprite.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SwapActiveSprite : MonoBehaviour
+{
+    [SerializeField]
+    private List<Sprite> sprites;
+
+    private int index;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void Swap()
+    {
+        index++;
+        if (index == sprites.Count)
+        {
+            index = 0;
+        }
+        spriteRenderer.sprite = sprites[index];
+    }
+}
