10317fdcf32b4f07f2ee79e3e61f5b98cd08e9e6
Ray Ryan <rayr@sgn.com>
2016-08-15 15:39:00 -0500

    SPOP-577 Woodstock animation state machine

---
+++ Assets/Scripts/Base/AnimationBehaviors/SetRandomIntegerParameter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SetRandomIntegerParameter : StateMachineBehaviour
+{
+    [SerializeField]
+    private string parameterName;
+
+    [SerializeField]
+    private int minValue;
+
+    [SerializeField]
+    private int maxValue;
+
+    public override void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
+    {
+        animator.SetInteger(parameterName, Random.Range(minValue, maxValue));
+    }
+}
