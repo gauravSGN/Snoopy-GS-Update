1b6590b32e0d7f7c5990c2b13b204c8b12cefe6f
Ray Ryan <rayr@sgn.com>
2016-08-19 11:04:10 -0500

    SPOP-594 Added AnimatedSequence and method to track items and determine when completed

---
+++ Assets/Scripts/Base/Sequence/SequenceItemCompleteEvent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Sequence
+{
+    sealed public class SequenceItemCompleteEvent : GameEvent
+    {
+        public GameObject item;
+
+        public SequenceItemCompleteEvent(GameObject item)
+        {
+            this.item = item;
+        }
+    }
+}
