6d151a0d4ed3615a409f1f2f99f47919a1a6a67c
Ray Ryan <rayr@sgn.com>
2016-08-25 18:50:44 -0500

    SPOP-682 Added some curvature to the new path

--- Assets/Scripts/Base/Paths/WindingPath.cs
+++ Assets/Scripts/Base/Paths/WindingPath.cs
@@ -5,22 +5,30 @@ namespace Paths
 {
     sealed public class WindingPath
     {
+        private struct ControlPoint
+        {
+            public Vector3 position;
+            public Vector3 tangent;
+        }
+
         private class Segment
         {
-            private Vector3 start;
-            private Vector3 end;
+            private Vector3[] controls = new Vector3[4];
 
             public int Index { get; private set; }
             public float Length { get; private set; }
             public float Position { get; private set; }
 
-            public void Initialize(List<Vector3> points, int index)
+            public void Initialize(List<ControlPoint> points, int index)
             {
                 Index = index;
                 Position = 0.0f;
-                start = points[index];
-                end = points[index + 1];
-                Length = (end - start).magnitude;
+                controls[0] = points[index].position;
+                controls[3] = points[index + 1].position;
+                Length = (controls[3] - controls[0]).magnitude;
+
+                controls[1] = controls[0] + points[index].tangent * (Length / 3.0f);
+                controls[2] = controls[3] - points[index + 1].tangent * (Length / 3.0f);
             }
 
             public float Advance(float distance)
@@ -35,19 +43,25 @@ namespace Paths
 
             public Vector3 Evaluate()
             {
-                return start + (end - start) * (Position / Length);
+                var t = Position / Length;
+                var inv = 1.0f - t;
+
+                return controls[0] * inv * inv * inv +
+                       controls[1] * t * inv * inv +
+                       controls[2] * t * t * inv +
+                       controls[3] * t * t * t;
             }
         }
 
-        private readonly List<Vector3> points = new List<Vector3>();
+        private readonly List<ControlPoint> points = new List<ControlPoint>();
         private readonly Segment current = new Segment();
 
         public bool Complete { get; private set; }
 
         public WindingPath(Vector3 start, Vector3 end)
         {
-            points.Add(start);
-            points.Add(end);
+            points.Add(new ControlPoint { position = start, tangent = Vector3.down });
+            points.Add(new ControlPoint { position = end, tangent = Vector3.down });
 
             current.Initialize(points, 0);
         }
