25cf93d57be4c11ac900b32d670ef2b43434a5f6
Ray Ryan <rayr@sgn.com>
2016-08-30 16:54:45 -0500

    SPOP-718 Updated various music to play through new SoundManager

---
+++ Assets/Scripts/Base/Sound/MusicPlayer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Sound
+{
+    sealed public class MusicPlayer : MonoBehaviour
+    {
+        [SerializeField]
+        private MusicType type;
+
+        [SerializeField]
+        private bool playOnAwake;
+
+        [SerializeField]
+        private bool loop;
+
+        public void Start()
+        {
+            if (playOnAwake)
+            {
+                Play();
+            }
+        }
+
+        public void Play()
+        {
+            StartCoroutine(PlayNextFrame());
+        }
+
+        private IEnumerator PlayNextFrame()
+        {
+            yield return null;
+
+            GlobalState.EventService.Dispatch(new PlayMusicEvent(type, loop));
+        }
+    }
+}
