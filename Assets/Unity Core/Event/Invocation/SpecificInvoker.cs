ddba2ae2fc567b8024f8a6a7ceec927b07273c55
Ray Ryan <rayr@sgn.com>
2016-08-31 16:29:58 -0500

    SPOP-776 Modifying EventDispatcher and friends to support parameterless listeners

---
+++ Assets/Unity Core/Event/Invocation/SpecificInvoker.cs
@@ -0,0 +1,13 @@
+﻿namespace Event.Invocation
+{
+    sealed public class SpecificInvoker<T> : Invoker where T : GameEvent
+    {
+        public object Target { get { return Handler; } }
+        public System.Action<T> Handler { get; set; }
+
+        public void Invoke(GameEvent gameEvent)
+        {
+            Handler.Invoke((T)gameEvent);
+        }
+    }
+}
