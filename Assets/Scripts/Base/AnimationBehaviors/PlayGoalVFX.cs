7ffcb157abd776764008dc3e8cea53465ac08995
Ray Ryan <rayr@sgn.com>
2016-09-08 16:06:03 -0500

    SPOP-725 Forgot to clean up from script template

--- Assets/Scripts/Base/AnimationBehaviors/PlayGoalVFX.cs
+++ Assets/Scripts/Base/AnimationBehaviors/PlayGoalVFX.cs
@@ -1,20 +1,17 @@
 ﻿using Service;
 using UnityEngine;
 
-namespace Namespace
+sealed public class PlayGoalVFX : StateMachineBehaviour
 {
-    sealed public class PlayGoalVFX : StateMachineBehaviour
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-        {
-            var transformService = GlobalState.Instance.Services.Get<TransformService>();
-            var goalIcon = transformService.Get(Registry.TransformUsage.RescueGoalIcon);
-            var iconAnimator = goalIcon.parent.GetComponent<Animator>();
+        var transformService = GlobalState.Instance.Services.Get<TransformService>();
+        var goalIcon = transformService.Get(Registry.TransformUsage.RescueGoalIcon);
+        var iconAnimator = goalIcon.parent.GetComponent<Animator>();
 
-            if (iconAnimator != null)
-            {
-                iconAnimator.SetTrigger("PlayVFX");
-            }
+        if (iconAnimator != null)
+        {
+            iconAnimator.SetTrigger("PlayVFX");
         }
     }
 }
