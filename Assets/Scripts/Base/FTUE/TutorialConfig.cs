d13f19e837658c83c4c8d2bc80b45e8255d6c066
Ray Ryan <rayr@sgn.com>
2016-09-14 14:56:05 -0500

    SPOP-541 Added TutorialService and config model

---
+++ Assets/Scripts/Base/FTUE/TutorialConfig.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace FTUE
+{
+    [Serializable]
+    sealed public class TutorialConfig
+    {
+        [Serializable]
+        sealed public class TutorialData
+        {
+            [SerializeField]
+            public string id;
+
+            [SerializeField]
+            public TutorialTrigger trigger;
+
+            [SerializeField]
+            public int level;
+
+            [SerializeField]
+            public bool skippable;
+
+            [SerializeField]
+            public string config;
+
+            [SerializeField]
+            public string reward;
+
+            [SerializeField]
+            public string text;
+        }
+
+        [Serializable]
+        sealed public class Reward
+        {
+            [SerializeField]
+            public string item;
+
+            [SerializeField]
+            public int count;
+        }
+
+        [Serializable]
+        sealed public class RewardList
+        {
+            [SerializeField]
+            public string id;
+
+            [SerializeField]
+            public List<Reward> rewards;
+        }
+
+        [SerializeField]
+        public List<TutorialData> tutorials;
+
+        [SerializeField]
+        public List<RewardList> rewardLists;
+    }
+}
---
+++ Assets/Scripts/Base/FTUE/TutorialManager.cs
@@ -0,0 +1,15 @@
+﻿using Service;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace FTUE
+{
+    sealed public class TutorialManager : TutorialService
+    {
+        public TutorialManager()
+        {
+            var configAsset = GlobalState.AssetService.LoadAsset<TextAsset>("tutorials");
+            var config = JsonUtility.FromJson<TutorialConfig>(configAsset.text);
+        }
+    }
+}
