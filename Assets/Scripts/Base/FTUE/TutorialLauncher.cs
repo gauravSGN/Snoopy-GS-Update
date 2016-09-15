4dfcc02595f1864cfe462b685ce3dd90e5597bcd
Ray Ryan <rayr@sgn.com>
2016-09-15 11:23:24 -0500

    SPOP-555 Adding launcher for tutorials

---
+++ Assets/Scripts/Base/FTUE/TutorialLauncher.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace FTUE
+{
+    sealed public class TutorialLauncher : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject overlay;
+
+        public void Start()
+        {
+            GlobalState.EventService.Persistent.AddEventHandler<ShowTutorialEvent>(OnShowTutorial);
+        }
+
+        private void OnShowTutorial(ShowTutorialEvent gameEvent)
+        {
+            overlay.SetActive(true);
+
+            var path = string.Format("Tutorials/{0}", gameEvent.id);
+            GlobalState.AssetService.LoadAssetAsync<GameObject>(path, OnTutorialLoaded);
+        }
+
+        private void OnTutorialLoaded(GameObject prefab)
+        {
+            Instantiate(prefab, transform);
+            overlay.SetActive(false);
+        }
+    }
+}
