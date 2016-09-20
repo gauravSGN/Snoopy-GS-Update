9114abc08d2d956074c4d2acf04866614d927a0d
Ray Ryan <rayr@sgn.com>
2016-09-20 16:30:49 -0500

    SPOP-771 Broke bubble placement out of LevelLoader and added placeholder boss mode placement

---
+++ Assets/Scripts/Snoopy/BossMode/BossBubblePlacer.cs
@@ -0,0 +1,94 @@
+﻿using Model;
+using UnityEngine;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Snoopy.BossMode
+{
+    sealed public class BossBubblePlacer : BubblePlacer
+    {
+        [SerializeField]
+        private Transform bubbleContainer;
+
+        private readonly List<Transform> bubbles = new List<Transform>();
+
+        override public void Start()
+        {
+            base.Start();
+
+            GlobalState.EventService.AddEventHandler<LevelLoadedEvent>(OnLevelLoaded);
+        }
+
+        override public GameObject PlaceBubble(BubbleData data)
+        {
+            var instance = CreateBubble(data);
+            var container = new GameObject();
+
+            container.transform.SetParent(bubbleContainer, false);
+            container.SetActive(false);
+            instance.transform.SetParent(container.transform, false);
+
+            container.transform.localPosition = GetBubbleLocation(data.X, data.Y);
+
+            bubbles.Add(instance.transform);
+
+            return instance;
+        }
+
+        private void OnLevelLoaded()
+        {
+            GlobalState.EventService.RemoveEventHandler<LevelLoadedEvent>(OnLevelLoaded);
+            GlobalState.EventService.Dispatch(new InputToggleEvent(false));
+
+            StartCoroutine(MoveBubbles());
+        }
+
+        private IEnumerator MoveBubbles()
+        {
+            var releaseRate = 3.0f / bubbles.Count;
+            var timer = 0.0f;
+            var active = 0;
+            var complete = 0;
+
+            bubbles.Sort((a, b) => b.localPosition.sqrMagnitude.CompareTo(a.localPosition.sqrMagnitude));
+
+            yield return null;
+
+            while (bubbles.Count > 0)
+            {
+                var deltaTime = Time.deltaTime;
+                timer += deltaTime;
+
+                while (((active - complete) < bubbles.Count) && (timer >= (active * releaseRate)))
+                {
+                    bubbles[active - complete].position = transform.position;
+                    bubbles[active - complete].parent.gameObject.SetActive(true);
+                    active++;
+                }
+
+                for (var index = 0; index < (active - complete); index++)
+                {
+                    var result = Vector3.MoveTowards(bubbles[index].localPosition, Vector3.zero, deltaTime);
+                    bubbles[index].localPosition = result;
+
+                    if (result.sqrMagnitude < Mathf.Epsilon)
+                    {
+                        var container = bubbles[index].parent;
+                        bubbles[index].SetParent(bubbleContainer, true);
+                        Destroy(container.gameObject);
+
+                        bubbles.RemoveAt(index);
+                        complete++;
+                        index--;
+                    }
+                }
+
+                yield return null;
+            }
+
+            GlobalState.EventService.Dispatch(new IntroScrollCompleteEvent());
+            GlobalState.EventService.Dispatch(new InputToggleEvent(true));
+        }
+    }
+}
