dadf3114774d288fd8f58bb7da9367066a2b8486
Ray Ryan <rayr@sgn.com>
2016-09-21 16:25:11 -0500

    Created an action queuing system for game objects

---
+++ Assets/Unity Core/Actions/Meta/LoopingActionList.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Actions.Meta
+{
+    sealed public class LoopingActionList : Actions.Base.TargettedAction
+    {
+        override public bool Done { get { return true; } }
+
+        private List<GameAction> actions;
+
+        public LoopingActionList(List<GameAction> actions)
+        {
+            this.actions = actions;
+        }
+
+        override public void Update()
+        {
+            var actionQueue = target.GetComponent<ActionQueue>();
+
+            foreach (var action in actions)
+            {
+                actionQueue.AddGameAction(action);
+            }
+
+            actionQueue.AddGameAction(this);
+        }
+    }
+}
