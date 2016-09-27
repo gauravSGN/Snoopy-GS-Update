63707b8f7ab20b8902dc5c12368ab78fd7176c2d
Ray Ryan <rayr@sgn.com>
2016-09-27 18:16:01 -0500

    SPOP-767 Making boss take damage when shot

--- Assets/Scripts/Snoopy/BossMode/BossController.cs
+++ Assets/Scripts/Snoopy/BossMode/BossController.cs
@@ -32,5 +32,20 @@ namespace Snoopy.BossMode
             var skinIndex = Mathf.RoundToInt((1.0f - (float)health / (float)maxHealth) * (damageSkins.Length - 1.0f));
             skeleton.SetSkin(damageSkins[skinIndex]);
         }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            var bubbleSnap = collision.collider.GetComponent<BubbleSnap>();
+
+            if (bubbleSnap != null)
+            {
+                health -= 1;
+
+                BubbleReactionEvent.Dispatch(Reaction.ReactionPriority.Cull, bubbleSnap.GetComponent<BubbleModelBehaviour>().Model);
+
+                bubbleSnap.CompleteSnap();
+                SetCurrentSkin();
+            }
+        }
     }
 }
