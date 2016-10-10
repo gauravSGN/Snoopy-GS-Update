d337c548d954582c04bdc26d29a748771399c283
gauravSGN <gaurav@sgn.com>
2016-10-10 13:47:54 -0700

    Add GS descriptor for Windows

--- Assets/Scripts/Base/Model/GSDescriptor/GSDescriptorFactory.cs
+++ Assets/Scripts/Base/Model/GSDescriptor/GSDescriptorFactory.cs
@@ -9,19 +9,23 @@ namespace Config
         private static Dictionary<RuntimePlatform, Type> platformClassMap = new Dictionary<RuntimePlatform, Type>
         {
             {RuntimePlatform.Android, typeof(AndroidGSDescriptor)},
-            {RuntimePlatform.IPhonePlayer, typeof(iOSGSDescriptor)}
+            {RuntimePlatform.IPhonePlayer, typeof(iOSGSDescriptor)},
+			{RuntimePlatform.WindowsPlayer, typeof(WindowsGSDescriptor)}
         };
 
         public static GSDescriptor CreateByPlatform(RuntimePlatform platform, TextAsset source)
         {
-            // We need to override our platform if we are using the editor
-            #if UNITY_EDITOR && UNITY_ANDROID
+			// We need to override our platform if we are using the editor
+#if UNITY_EDITOR && UNITY_ANDROID
                 platform = RuntimePlatform.Android;
-            #elif UNITY_EDITOR && UNITY_IOS
+#elif UNITY_EDITOR && UNITY_IOS
                 platform = RuntimePlatform.IPhonePlayer;
-            #endif
-
-            return (GSDescriptor)Activator.CreateInstance(platformClassMap[platform], new System.Object[] { source });
+#elif UNITY_EDITOR && UNITY_STANDALONE_WIN
+				platform = RuntimePlatform.WindowsPlayer;
+#elif UNITY_STANDALONE_WIN
+				platform = RuntimePlatform.WindowsPlayer;
+#endif
+			return (GSDescriptor)Activator.CreateInstance(platformClassMap[platform], new System.Object[] { source });
         }
     }
 }
---
+++ Assets/Scripts/Base/Model/GSDescriptor/WindowsGSDescriptor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Config
+{
+	public class WindowsGSDescriptor : GSDescriptor
+	{
+		public WindowsGSDescriptor(TextAsset gsDescriptorJSON) : base(gsDescriptorJSON) { }
+
+		override protected void InitializePlatformValues()
+		{
+			Contents["store"] = "windows";
+			Contents["osVersion"] = "windows";
+			Contents["platform"] = "windows";
+		}
+	}
+}
